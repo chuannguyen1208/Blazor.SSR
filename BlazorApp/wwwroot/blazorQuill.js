42fd86d3d0578f27f5fb29cc0389703ae67bbc1a
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-11-25 23:31:26 +0700

    complete show review

--- BlazorApp/wwwroot/blazorQuill.js
+++ BlazorApp/wwwroot/blazorQuill.js
@@ -5,23 +5,21 @@
         console.log('quill init...');
 
         var toolbarOptions = [
-            ['bold', 'italic', 'underline', 'strike'],          // toggled buttons
-            ['blockquote', 'code-block'],
-
-            [{ 'header': 1 }, { 'header': 2 }],                 // custom button values
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            [{ 'script': 'sub' }, { 'script': 'super' }],       // superscript/subscript
-            [{ 'indent': '-1' }, { 'indent': '+1' }],           // outdent/indent
-            [{ 'direction': 'rtl' }],                           // text direction
-
-            [{ 'size': ['small', false, 'large', 'huge'] }],    // custom dropdown
+            ['bold', 'italic', 'underline', 'strike'],
             [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            ['blockquote', 'code-block'],
+            ['link', 'image'],
+            [{ 'color': [] }, { 'background': [] }],
 
-            [{ 'color': [] }, { 'background': [] }],            // dropdown with defaults from theme
-            [{ 'font': [] }],
-            [{ 'align': [] }],
-
-            ['clean']                                            // remove formatting button
+            //[{ 'font': [] }],
+            //[{ 'header': 1 }, { 'header': 2 }],
+            //[{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            //[{ 'script': 'sub' }, { 'script': 'super' }],
+            //[{ 'indent': '-1' }, { 'indent': '+1' }],
+            //[{ 'direction': 'rtl' }],
+            //[{ 'size': ['small', false, 'large', 'huge'] }],
+            //[{ 'align': [] }],
+            //['clean']
         ];
 
         this.quill = new Quill(element, {
@@ -34,6 +32,10 @@
 
     static getContent() {
         return this.quill.root.innerHTML;
+    }
+
+    static showContent(elementRef) {
+        elementRef.innerHTML = this.getContent();
     }
 }
 
